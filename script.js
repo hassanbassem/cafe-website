a83a9ab4d72f86e4238107bc996a5a65c908dd0a
hassan <hassona1392004@gmail.com>
2025-09-08 05:51:46 +0300

    made the footer responsive

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const burgerMenu = document.getElementById("burger-menu");
 const nav = document.querySelector("nav");
+const reverseElement = document.querySelector(".reverse");
 
 burgerMenu.onclick = () => nav.classList.toggle("visible");
+
+const resizeHandler = () => {
+    if (innerWidth > 770){
+        reverseElement.classList.add("reverse");
+        reverseElement.classList.add("rtl");
+    }
+    else{
+        reverseElement.classList.remove("reverse");
+        reverseElement.classList.remove("rtl");
+
+    }
+}
+
+resizeHandler();
+
+addEventListener("resize", resizeHandler);
